df495b9b15f0796f24f535728b9a64f638a58e00
Florencia Varela <florenciavarelasc@gmail.com>
2026-06-20 17:53:35 -0300

    Revert "37-Implement profile edit functionality and validations"

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { AuthService, AuthUser } from '../services/auth.service';
@@ -25,16 +24,6 @@ export class ConfigComponent implements OnInit {
     role: 'Administrador',
   };
 
-  userProfile: { phone: string; address: string; birthdate: string } = {
-    phone: '',
-    address: '',
-    birthdate: ''
-  };
-
-  isEditing = false;
-  maxDate: string = '';
-  minDate: string = '';
-
   // App Info
   appInfo = {
     name: 'Intelligent Hospital Management System (IHMS)',
@@ -42,28 +31,8 @@ export class ConfigComponent implements OnInit {
     userAgent: '',
   };
 
-  get formattedBirthdate(): string {
-    if (!this.userProfile.birthdate) return '';
-    const [year, month, day] = this.userProfile.birthdate.split('-');
-    return `${day}/${month}/${year}`;
-  }
-
   ngOnInit() {
     this.appInfo.userAgent = navigator.userAgent;
-
-    const today = new Date();
-    const maxYear = today.getFullYear() - 16;
-    const minYear = today.getFullYear() - 100;
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
-    
-    this.maxDate = `${maxYear}-${month}-${day}`;
-    this.minDate = `${minYear}-${month}-${day}`;
-
-    const savedProfile = localStorage.getItem('userProfile');
-    if (savedProfile) {
-      this.userProfile = JSON.parse(savedProfile);
-    }
 
     this.authService.user$.subscribe((userData: AuthUser | null) => {
       if (userData) {
@@ -75,23 +44,6 @@ export class ConfigComponent implements OnInit {
         this.router.navigate(['/login']);
       }
     });
-  }
-
-  editProfile() {
-    this.isEditing = true;
-  }
-
-  cancelEdit() {
-    this.isEditing = false;
-    const savedProfile = localStorage.getItem('userProfile');
-    if (savedProfile) {
-      this.userProfile = JSON.parse(savedProfile);
-    }
-  }
-
-  saveProfile() {
-    localStorage.setItem('userProfile', JSON.stringify(this.userProfile));
-    this.isEditing = false;
   }
 
   async logout() {
