e5111d42e066304c9ab84460cf6a12481b186586
totomonaco <tomymonaco1@gmail.com>
2026-07-13 15:03:45 -0300

    show spinner on button while checking session state

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { AuthService } from '../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -11,18 +12,34 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   isLoading = signal(false);
+  isCheckingSession = signal(true);
 
   private router = inject(Router);
   private authService = inject(AuthService);
+  private sessionSub?: Subscription;
 
-  constructor() {
-    this.authService.isLogged$.subscribe((isLogged) => {
-      if (isLogged) {
-        this.router.navigate(['/main/items']);
-      }
+  ngOnInit(): void {
+    this.sessionSub = this.authService.isLogged$.subscribe({
+      next: (isLogged) => {
+        if (isLogged) {
+          this.router.navigate(['/main/items']);
+        } else {
+          this.isCheckingSession.set(false);
+        }
+      },
+      error: (err) => {
+        console.error('Error checking session state:', err);
+        this.isCheckingSession.set(false);
+      },
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.sessionSub) {
+      this.sessionSub.unsubscribe();
+    }
   }
 
   async login(): Promise<void> {
