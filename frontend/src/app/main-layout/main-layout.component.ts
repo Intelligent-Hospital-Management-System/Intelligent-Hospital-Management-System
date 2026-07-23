150746cfaae9dd60797cd935f072c632c2cb6fc0
totomonaco <tomymonaco1@gmail.com>
2026-07-23 20:50:52 -0300

    move project to frontend directory

--- frontend/src/app/main-layout/main-layout.component.ts
+++ frontend/src/app/main-layout/main-layout.component.ts
@@ -7,7 +7,7 @@ import { RouterModule, Router } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.css']
+  styleUrls: ['./main-layout.component.css'],
 })
 export class MainLayoutComponent implements OnInit {
   constructor(private router: Router) {}
